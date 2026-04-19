85db845582b127edbf01dbc03b6af95e72cfd8f4
AndrewSerulneck <AndrewSerulneck@users.noreply.github.com>
2026-04-19 10:04:43 -0400

    Remove top and bottom inline ads from predictions page

--- app/predictions/page.tsx
+++ app/predictions/page.tsx
@@ -1,6 +1,5 @@
 import { PageShell } from "@/components/ui/PageShell";
 import { PredictionMarketList } from "@/components/predictions/PredictionMarketList";
-import { InlineSlotAdClient } from "@/components/ui/InlineSlotAdClient";
 import { BackButton } from "@/components/navigation/BackButton";
 import { APP_PAGE_NAMES } from "@/lib/pageNames";
 
@@ -12,21 +11,7 @@ export default async function PredictionsPage() {
     >
       <div className="space-y-4">
         <BackButton label="Back" />
-        <InlineSlotAdClient
-          slot="inline-content"
-          showPlaceholder
-          showPlacementDebug
-          placeholderLabel="Ad Space #3101"
-          placeholderDetails="Predictions page top inline slot"
-        />
         <PredictionMarketList />
-        <InlineSlotAdClient
-          slot="mid-content"
-          showPlaceholder
-          showPlacementDebug
-          placeholderLabel="Ad Space #3102"
-          placeholderDetails="Predictions page bottom inline slot"
-        />
       </div>
     </PageShell>
   );
